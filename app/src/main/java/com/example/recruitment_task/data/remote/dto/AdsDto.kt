2ee42c158b466c45be8b18187e0003e2e63fcd4a
Krzysztof Lewiński <cotek1993@wp.pl>
2025-12-06 11:25:29 +0100

    Remove response nullable

--- app/src/main/java/com/example/recruitment_task/data/remote/dto/AdsDto.kt
+++ app/src/main/java/com/example/recruitment_task/data/remote/dto/AdsDto.kt
@@ -7,7 +7,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class AdsDto(
     @SerialName("items")
-    val items: List<AdDto>?,
+    val items: List<AdDto>,
     @SerialName("size")
-    val size: Int?
+    val size: Int
 )
