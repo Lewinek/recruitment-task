05a1cf206df2d6852eb1a292aacb572eead1653d
Krzysztof Lewiński <cotek1993@wp.pl>
2025-12-06 09:14:24 +0100

    Add ResultWrapper for api response handling

---
+++ app/src/main/java/com/example/recruitment_task/util/ResultWrapper.kt
@@ -0,0 +1,22 @@
+package com.example.recruitment_task.util
+
+
+
+sealed class ResultWrapper<out T> {
+    object Loading : ResultWrapper<Nothing>()
+    data class Success<out T>(val data: T) : ResultWrapper<T>()
+    data class Error<T>(val message: String, val exception: Throwable? = null) : ResultWrapper<Nothing>()
+}
+
+inline fun <T> ResultWrapper<T>.onSuccess(action: (T) -> Unit) : ResultWrapper<T>{
+    if (this is ResultWrapper.Success) action(data)
+    return this
+}
+
+inline fun <T> ResultWrapper<T>.onError(action: (String) -> Unit) : ResultWrapper<T>{
+    if (this is ResultWrapper.Error<*>) action(message)
+    return this
+}
+
+
+
