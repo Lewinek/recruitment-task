c20a4eba457a7d5eb0fc862dfd84e7d0577a5ab6
Krzysztof Lewiński <cotek1993@wp.pl>
2025-12-06 14:42:24 +0100

    Add favorites method to repository implementation

--- app/src/main/java/com/example/recruitment_task/data/repository/AdRepositoryImpl.kt
+++ app/src/main/java/com/example/recruitment_task/data/repository/AdRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package com.example.recruitment_task.data.repository
 
+import com.example.recruitment_task.data.local.dao.FavoriteAdDao
+import com.example.recruitment_task.data.local.mapper.toDomain
 import com.example.recruitment_task.data.remote.MartketplaceApi
 import com.example.recruitment_task.data.remote.mapper.toDomain
 import com.example.recruitment_task.domain.model.Ad
@@ -7,9 +9,11 @@ import com.example.recruitment_task.domain.repository.AdRepository
 import com.example.recruitment_task.util.ResultWrapper
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 
 class AdRepositoryImpl(
-   private val api: MartketplaceApi
+   private val api: MartketplaceApi,
+    private val favoriteAdDao: FavoriteAdDao
 ) : AdRepository {
     override fun getAds(): Flow<ResultWrapper<List<Ad>>> = flow {
         emit(ResultWrapper.Loading)
@@ -20,5 +24,9 @@ class AdRepositoryImpl(
         } catch (e: Exception){
             emit(ResultWrapper.Error("Failed to load ads: ${e.message}", e))
         }
+    }
+
+    override fun getFavouritesAds(): Flow<List<Ad>> {
+       return favoriteAdDao.getAllFavouritesAds().map { entities -> entities.map { it.toDomain() } }
     }
 }
