b895fa4d64c435a8a639c61542b4e847ca328204
Krzysztof Lewiński <cotek1993@wp.pl>
2025-12-06 14:38:23 +0100

    Fix missing favourite DAO Query annotation

--- app/src/main/java/com/example/recruitment_task/data/local/dao/FavoriteAdDao.kt
+++ app/src/main/java/com/example/recruitment_task/data/local/dao/FavoriteAdDao.kt
@@ -1,11 +1,13 @@
 package com.example.recruitment_task.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Query
 import com.example.recruitment_task.data.local.entity.FavoriteAdEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface FavoriteAdDao {
 
+    @Query("SELECT * FROM favorites_ads")
     fun getAllFavouritesAds() : Flow<List<FavoriteAdEntity>>
 }
