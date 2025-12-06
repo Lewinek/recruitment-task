46a762072e91d202eed15d3b8ae8f2027795364e
Krzysztof Lewiński <cotek1993@wp.pl>
2025-12-06 09:38:31 +0100

    Add Retrofit Koin dependencies

--- app/src/main/java/com/example/recruitment_task/di/AppModule.kt
+++ app/src/main/java/com/example/recruitment_task/di/AppModule.kt
@@ -1,7 +1,29 @@
 package com.example.recruitment_task.di
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val appModule = module {
 
+    single {
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    single {
+        OkHttpClient.Builder()
+            .addInterceptor(get<HttpLoggingInterceptor>())
+            .build()
+    }
+
+    single {
+        Retrofit.Builder()
+            .baseUrl("https://gist.githubusercontent.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
