63ad0d962b20423d865cd892e337204b5aa97425
Krzysztof Lewiński <cotek1993@wp.pl>
2025-12-06 10:12:01 +0100

    Add data source layer for ads: mapper, AdRepository, Retrofit API interface

---
+++ app/src/main/java/com/example/recruitment_task/data/remote/mapper/AdMapper.kt
@@ -0,0 +1,15 @@
+package com.example.recruitment_task.data.remote.mapper
+
+import com.example.recruitment_task.data.remote.dto.AdDto
+import com.example.recruitment_task.domain.model.Ad
+
+
+fun AdDto.toDomain(): Ad {
+    return Ad(
+        id = id,
+        title = description,
+        image = image.url,
+        price = price.value,
+        location = location
+    )
+}
